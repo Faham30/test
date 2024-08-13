fc2c57c5a864ee13e46909b1067616e47e91078a
Faham30 <taifaham2330@gmail.com>
2024-08-13 20:10:47 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,244 @@
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a , "& b = ", b)
+// console.log("a + b = ", a + b);
+// console.log("a  b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ",a," & b = ", b);
+
+// console.log("a++ = ", a++);
+// console.log("a = ", a)
+
+
+
+//assignment operator
+// let a = 5;
+// let b = 2;
+
+// a += 4;
+// console.log("a = ", a)
+
+
+
+//LOGICAL OPERATORS
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b;
+// let cond2 = a === 6;
+// console.log("cond1 || cond2 = ", cond1 || cond2);
+
+
+//CONDITIONAL STATEMENTS:
+
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// }else {
+//     color = "white";
+// }
+
+
+
+// console.log(color);
+
+
+// let age = 16;
+
+// if (age >= 18) {
+//     console.log("vote")
+// } else {
+//     console.log("not vote")
+// }
+
+
+//ODD OR EVEN NUMBER CHAKE:
+// let num = 17;
+
+// if (num%2 === 0) {
+//     console.log(num, "is even");
+// } else {
+//     console.log(num, "is odd");
+// }
+
+//else if statement :
+
+// let mode ="darkvb ";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// } else if (mode == "blue") {
+//     color = "blue";
+// } else if (mode === "pink") {
+//     color = "pink";
+// } else {
+//     color = "white";
+// }
+
+// console.log(color);
+
+// if (mode == "dark") console.log(mode);
+
+
+//OPERATORS (TERNARY):
+// let age = 25;
+
+// let result = age >= 18 ? "adult" : "not adult";
+// console.log(result);
+
+//  let num = prompt("enter a number:");
+// if (num % 5 === 0 ) {
+//     console.log(num , "num is multiple of 5");
+// } else {
+//     console.log(num, "is not multiple of 5");
+// }
+
+
+//practice qu02
+
+// let score = prompt("enter a ypur score (0-100)");
+// let grade;
+
+
+// if (score >= 90 && score <= 100) {
+//     grade = "A";
+// } else if (score >= 70 && score <= 89){
+//     grade = "B";
+// } else if (score >= 60 && score <= 69) {
+//     grade = "C";
+// } else if (score >= 50 && score <= 59) {
+//     grade = "D";
+// } else if (score >= 0 && score <= 49) {
+//     grade = "F";
+// }
+
+// console.log("according to your score, your grade was : ", grade)
+
+//LOOPS:
+
+
+// for(let count=1; count<=5;count++) {
+//     console.log("faham");
+// }
+
+//CALCULATE SUM OF 1 TO 5 :
+// let sum = 0;
+// let n = 100;
+// for(let i = 1; i<=n;i++){
+//     sum = sum + i;
+// }
+// console.log("sum = ", sum);
+
+// for(let i=1; i<=5;i++) {
+//     console.log("i = ",i);
+// }
+
+// WHILE LOOPS:
+
+// let i = 1;
+// while(i<=10){
+//     console.log("i=",i)
+//     i++;
+// }
+
+//DO-WHILE LOOP:
+// let i = 1;
+// do {
+//     console.log("faham")
+//     i++;
+// } while (i<=10);
+
+//PRACTICE QU2:
+// let gameNum = 25;
+
+// let userNum = prompt("Guess the game number : ");
+// console.log(gameNum);
+
+// while(userNum != gameNum){
+//     userNum = prompt("You enter wrong number ,Guess again : ");
+// }
+// console.log("congratulation, you entered the right number");
+
+// let cities = ["ahmedabad" , "surat", "delhi", "mumbai", "pune"];
+
+// for (let city of cities) {
+//     console.log(city);
+// }
+
+
+
+
+//functions:
+// function sum(x,y) {
+//     s = x + y;
+//     console.log("before return");
+//     return s;
+//     console.log("after return")
+// }
+// let val = sum(3,4);
+// console.log(val);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
